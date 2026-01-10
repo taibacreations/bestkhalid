6817ce4f83bb9e736197f6df8c353d7708b36383
Mr-Shehroz <shehrozkhan.wow@gmail.com>
2026-01-10 13:32:47 +0500

    fix the lenis error

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -11,12 +11,7 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // smooth easing
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
       smoothWheel: true,
-      smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
     });
